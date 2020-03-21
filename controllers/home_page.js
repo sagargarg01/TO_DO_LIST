8c39b2b381ba8f261d1da8b2be835f8dfd6478e9
Sagar Garg <sagargupta1417@gmail.com>
2020-03-21 19:10:33 +0530

    completed route and controller and setup the view engine

--- controllers/home_page.js
+++ controllers/home_page.js
@@ -1,5 +1,5 @@
 module.exports.homepage = function(req,res){
-    return res.render('home_page',{
+    return res.render('home',{
        title : "TO_DO_LIST" 
     });
 }
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 
 const router = express.Router();
+const homePage  = require('../controllers/home_page');
 
 console.log('router loaded');
+
+router.get('/',homePage.homepage);
 
 module.exports = router;
